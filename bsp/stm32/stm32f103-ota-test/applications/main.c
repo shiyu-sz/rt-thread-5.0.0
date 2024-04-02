83d81636902c5e904a150820b9722e4c8fb91c44
shiyu <971586331@qq.com>
2024-04-02 16:17:52 +0800

    在stm32f103c8t6上正常运行

--- bsp/stm32/stm32f103-ota-test/applications/main.c
+++ bsp/stm32/stm32f103-ota-test/applications/main.c
@@ -12,19 +12,16 @@
 #include <rtdevice.h>
 #include <board.h>
 
-/* defined the LED0 pin: PC0 */
-#define LED0_PIN    GET_PIN(C, 0)
-
 int main(void)
 {
     /* set LED0 pin mode to output */
-    rt_pin_mode(LED0_PIN, PIN_MODE_OUTPUT);
+    rt_pin_mode(GET_PIN(C, 13), PIN_MODE_OUTPUT);
 
     while (1)
     {
-        rt_pin_write(LED0_PIN, PIN_HIGH);
+        rt_pin_write(GET_PIN(C, 13), PIN_HIGH);
         rt_thread_mdelay(500);
-        rt_pin_write(LED0_PIN, PIN_LOW);
+        rt_pin_write(GET_PIN(C, 13), PIN_LOW);
         rt_thread_mdelay(500);
     }
 }
