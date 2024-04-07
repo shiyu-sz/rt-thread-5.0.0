9267f449173b487218299a665a5afab46ade7c56
shiyu <971586331@qq.com>
2024-04-07 17:27:34 +0800

    usb主机挂载u盘失败

---
+++ bsp/stm32/stm32f429-usb-host/applications/main.c
@@ -0,0 +1,93 @@
+/*
+ * Copyright (c) 2006-2021, RT-Thread Development Team
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ * 2018-11-06     SummerGift   first version
+ */
+
+#include <rtthread.h>
+#include <rtdevice.h>
+#include <board.h>
+
+/* defined the LED0 pin: PB1 */
+#define LED0_PIN    GET_PIN(B, 1)
+
+#include <dfs_posix.h>
+#define TEST_FN     "/test_usbh.c"
+static char test_data[120], buffer[120];
+
+void readwrite(const char* filename)
+{
+    int fd;
+    int index, length;
+
+    fd = open(TEST_FN, O_WRONLY | O_CREAT | O_TRUNC, 0);
+    if (fd < 0)
+    {
+        rt_kprintf("open file for write failed\n");
+        return;
+    }
+
+    for (index = 0; index < sizeof(test_data); index ++)
+    {
+        test_data[index] = index + 27;
+    }
+
+    length = write(fd, test_data, sizeof(test_data));
+    if (length != sizeof(test_data))
+    {
+        rt_kprintf("write data failed\n");
+        close(fd);
+        return;
+    }
+
+    close(fd);
+
+    fd = open(TEST_FN, O_RDONLY, 0);
+    if (fd < 0)
+    {
+        rt_kprintf("check: open file for read failed\n");
+        return;
+    }
+
+    length = read(fd, buffer, sizeof(buffer));
+    if (length != sizeof(buffer))
+    {
+        rt_kprintf("check: read file failed\n");
+        close(fd);
+        return;
+    }
+
+    for (index = 0; index < sizeof(test_data); index ++)
+    {
+        if (test_data[index] != buffer[index])
+        {
+            rt_kprintf("check: check data failed at %d\n", index);
+            close(fd);
+            return;
+        }
+    }
+
+    rt_kprintf("usb host read/write udisk successful\r\n");
+
+    close(fd);
+}
+
+MSH_CMD_EXPORT(readwrite, usb host read write test);
+
+int main(void)
+{
+    /* set LED0 pin mode to output */
+    rt_pin_mode(LED0_PIN, PIN_MODE_OUTPUT);
+
+    while (1)
+    {
+        rt_pin_write(LED0_PIN, PIN_HIGH);
+        rt_thread_mdelay(500);
+        rt_pin_write(LED0_PIN, PIN_LOW);
+        rt_thread_mdelay(500);
+    }
+}
